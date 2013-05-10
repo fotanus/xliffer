9240d3da18fe52a59cd5ceee57604c00d3314fbe
Felipe de Oliveira Tanus <fotanus@gmail.com>
2013-05-09 22:51:02 -0300

    better descriptions on spec

--- spec/xliff_file_spec.rb
+++ spec/xliff_file_spec.rb
@@ -10,19 +10,19 @@ describe XLIFFer::XLIFFFile do
       XLIFFer::XLIFFFile.new(File.open("spec/files/empty.xliff").read).should be
     end
 
-    it "don't accepts a number" do
+    it "doesn't accept a number" do
       expect{XLIFFer::XLIFFFile.new(123)}.to raise_error ArgumentError
     end
 
-    it "don't accepts a random string" do
+    it "doesn't accept a random string" do
       expect{XLIFFer::XLIFFFile.new("foobar")}.to raise_error XLIFFer::FormatError
     end
 
-    it "don't accepts a random file" do
+    it "doesn't accept a random file" do
       expect{XLIFFer::XLIFFFile.new("file.foobar")}.to raise_error XLIFFer::FormatError
     end
 
-    context "parsing attributes" do
+    context "parsing version" do
       it "get the xliff version" do
          XLIFFer::XLIFFFile.new('<xliff version="9.8"></xliff>').version.should eql("9.8")
       end
